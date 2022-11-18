9cce8151cfb94c18cb8dbd5c4b234a89b545c42e
Layla Ventillari <laylaventilari5@gmail.com>
2022-11-18 19:17:31 -0300

    header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,13 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import logoImg from "../../assets/logo.svg";
 export function Header(){
   return(
-    <div>
-      
-    </div>
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+
+        <NewTransactionButton>Nova Transação</NewTransactionButton>
+      </HeaderContent>
+    </HeaderContainer>
   )
 }
