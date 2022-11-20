013977fc659b9481c2f25741dffc4124ab408350
Layla Ventillari <laylaventilari5@gmail.com>
2022-11-19 23:22:43 -0300

    final

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,9 @@ import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton }
 import * as z from 'zod';
 import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod';
+import { api } from '../../lib/axios';
+import { useContext } from 'react';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
@@ -15,19 +18,29 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema >
 
 export function NewTransactionModal(){
+  const { createTransaction } = useContext(TransactionsContext)
   const {
     control,
     register, 
     handleSubmit,
-    formState: {isSubmitting}
+    formState: {isSubmitting},
+    reset,
+
    } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(newTransactionFormSchema),
     })
   async function handleCreateNewTransaction(data: NewTransactionFormInputs){
-    await new Promise(resolve => setTimeout(resolve, 2000));  
-    console.log(data)
-    }
-  
+    const { description, price, category, type } = data;
+
+    await createTransaction({
+      description,
+      price,
+      category,
+      type,
+    })
+    
+    reset();
+  }
   return(
     <Dialog.Portal>
       <Overlay />
